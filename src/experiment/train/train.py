9ffcf17dc9bcc52fd8aca29ecaf7bca430cea318
Mizuki <mbaba@kanolab.net>
2024-09-14 16:02:48 +0900

    feat: 単GPUを用いた学習スクリプトの定義

--- src/experiment/train/train.py
+++ src/experiment/train/train.py
@@ -1,35 +1,63 @@
+import os
+
 import torch
+import torch.multiprocessing as mp
 import torch.optim as optim
 from dacite import Config as DaciteConfig
 from dacite import from_dict
 from omegaconf import OmegaConf
+from torch.distributed.fsdp import FullyShardedDataParallel as FSDP
+from torch.distributed.fsdp import ShardingStrategy
+from torch.distributed.fsdp.wrap import size_based_auto_wrap_policy
 from torch.utils.data import DataLoader
+<<<<<<< Updated upstream
+=======
+from torch.utils.data.distributed import DistributedSampler
+from transformers import set_seed
+>>>>>>> Stashed changes
 from xlstm import xLSTMLMModel, xLSTMLMModelConfig
 
+import wandb
 from src.cfg.load_yaml_cfg import load_config
 from src.dataset.nlp_dataset import NlpDatasetGenerator
 from src.experiment.train.lr_scheduler import LinearWarmupCosineAnnealing
 from src.experiment.train.trainer import Trainer
+<<<<<<< Updated upstream
 from src.utils import torch_dtype_map
 
 
 def main():
     config = load_config("src/cfg/yaml/test_config.yaml")
+=======
+from src.utils import dist_cleanup, dist_setup, torch_dtype_map, wandb_init
+
+os.environ["TOKENIZERS_PARALLELISM"] = "false"
+
+
+def main(config):
+    wandb_init(config)
+    set_seed(config.basic.seed)
+>>>>>>> Stashed changes
+
+    pad_token_id = config.dataset.pad_token_id
 
     dataset = NlpDatasetGenerator(config)
+    train_loader = DataLoader(
+        dataset.train,
+        batch_size=config.training.batch_size,
+        drop_last=True,
+        num_workers=1,
+        pin_memory=True,
+        shuffle=False,
+    )
     print("Dataset loaded! ")
     print(f"Train dataset size: {len(dataset.train)}")
-    train_loader = DataLoader(
-        dataset.train, batch_size=config.training.batch_size, drop_last=True
-    )
-    pad_token_id = config.dataset.pad_token_id
 
     model_config = from_dict(
         data_class=xLSTMLMModelConfig,
         data=OmegaConf.to_container(config.model, resolve=True),
         config=DaciteConfig(strict=True),
     )
-
     model = xLSTMLMModel(model_config)
     model.reset_parameters()
     model.train()
@@ -37,12 +65,15 @@ def main():
     model = model.to(config.basic.device)
 
     optim_groups = model._create_weight_decay_optim_groups()
+
     optimizer = optim.AdamW(
         (
             {"weight_decay": config.training.weight_decay, "params": optim_groups[0]},
             {"weight_decay": 0.0, "params": optim_groups[1]},
         ),
         lr=config.training.lr,
+        betas=(0.9, 0.95),
+        eps=1e-5,
     )
 
     lr_scheduler = LinearWarmupCosineAnnealing(
@@ -55,10 +86,17 @@ def main():
 
     criterion = torch.nn.CrossEntropyLoss(ignore_index=pad_token_id)
 
-    trainer = Trainer(model, train_loader, lr_scheduler, optimizer, criterion, config)
+    trainer = Trainer(
+        model, train_loader, lr_scheduler, optimizer, criterion, config, rank=0
+    )
     trainer.train()
     print("Training finished!")
+    wandb.finish()
 
 
 if __name__ == "__main__":
-    main()
+    config = load_config("src/cfg/yaml/v4/train_config.yaml")
+    try:
+        main(config)
+    except Exception as e:
+        print(e)
--- src/experiment/train/trainer.py
+++ src/experiment/train/trainer.py
@@ -2,13 +2,23 @@ import math
 import os
 
 import torch
+<<<<<<< Updated upstream
+=======
+import torch.distributed as dist
+from torch.distributed.fsdp import FullStateDictConfig
+from torch.distributed.fsdp import FullyShardedDataParallel as FSDP
+from torch.distributed.fsdp import StateDictType
+>>>>>>> Stashed changes
 from tqdm import tqdm
 
+import wandb
 from src.utils import torch_dtype_map
 
 
 class Trainer:
-    def __init__(self, model, train_loader, lr_scheduler, optimizer, criterion, config):
+    def __init__(
+        self, model, train_loader, lr_scheduler, optimizer, criterion, config, rank
+    ):
         print("Trainer initializing...")
         self.model = model
         self.train_loader = train_loader
@@ -16,26 +26,29 @@ class Trainer:
         self.optimizer = optimizer
         self.criterion = criterion
         self.config = config
+        self.rank = rank
 
         self.step = 0
         self.epoch = 1
         self.train_step_num = len(train_loader)
-        self.running_loss = float("inf")
+        self.running_loss = torch.tensor(0.0).to(self.rank)
         print(f"Train Step Num: {self.train_step_num}")
 
     def train(self):
         # TODO: loggingに変更
-        print("Start training!")
-        monitoring = tqdm(self.train_loader, total=0, initial=0)
+        monitoring = tqdm(
+            self.train_loader, desc=f"#{self.rank:>2} ", position=self.rank + 1
+        )
         for _ in range(self.config.training.num_epochs):
             print(f"######### Epoch {self.epoch} #########")
             for batch in monitoring:
                 monitoring.set_description_str(
-                    f"Steps {self.step+1}/{self.train_step_num} (Loss: {self.running_loss:.4f})"
+                    f"Rank {self.rank} Steps {self.step+1}/{self.train_step_num} (Loss: {self.running_loss:.4f})"
                 )
                 self.train_step(batch)
                 self.step += 1
                 if self.step % self.config.training.val_every_step == 0:
+<<<<<<< Updated upstream
                     print(
                         f"\nStep [{self.step+1}/{self.train_step_num}] (Epoch: {self.epoch}), Loss: {self.running_loss:.4f},"
                     )
@@ -43,12 +56,19 @@ class Trainer:
                         f"{self.config.training.model_save_dir}/model_{self.step}.pth"
                     )
             self.epoch += 1
+=======
+                    self.valid_step()
+                self.step += 1
+            self.epoch += 1
+            wandb.alert(
+                title="Epoch Done",
+                text=f"Epoch {self.epoch-1} is done! Loss: {self.running_loss}",
+            )
+>>>>>>> Stashed changes
 
     def train_step(self, batch):
         feature, label = batch["feature"], batch["label"]
-        feature, label = feature.to(self.config.basic.device), label.to(
-            self.config.basic.device
-        )
+        feature, label = feature.to(self.rank), label.to(self.rank)
         with torch.autocast(
             device_type=self.config.basic.device,
             dtype=torch_dtype_map[self.config.training.amp_precision],
@@ -67,7 +87,30 @@ class Trainer:
             if math.isnan(self.running_loss):
                 raise ValueError("Loss is NaN!")
 
+<<<<<<< Updated upstream
     def save_model(self, path):
         os.makedirs(os.path.dirname(path), exist_ok=True)
         torch.save(self.model.state_dict(), path)
         print(f"Model has been saved to {path}")
+=======
+    def valid_step(self):
+        self.save_model(f"{self.config.training.model_save_dir}/model_{self.step}.pth")
+
+    def save_model(self, save_path):
+        # Save checkpoints.
+        state_dict = self.model.state_dict()
+        os.makedirs(os.path.dirname(save_path), exist_ok=True)
+        # This name is needed for HF from_pretrained API to work.
+        torch.save(state_dict, save_path)
+        print(f"Model has been saved to {save_path}")
+
+    def step_logging(self):
+        wandb.log(
+            {
+                "Loss": self.running_loss,
+                "Learning Rate": self.optimizer.param_groups[0]["lr"],
+                "Epoch": self.epoch,
+                "Step": self.step,
+            }
+        )
+>>>>>>> Stashed changes
