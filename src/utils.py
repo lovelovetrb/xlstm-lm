1e8c98116076f57f6629eebf4d5cd4c91a18a83e
Mizuki <mbaba@kanolab.net>
2024-09-14 15:57:55 +0900

    feat: distributedの初期化と終了処理を行う関数の定義

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,6 @@
 import torch
+import torch.distributed as dist
+
 import wandb
 
 torch_dtype_map: dict[str, torch.dtype] = {
@@ -9,4 +11,16 @@ torch_dtype_map: dict[str, torch.dtype] = {
 
 
 def wandb_init(config):
-    wandb.init(project="xlstm_train", config=config)
+    wandb.init(
+        project="xlstm_train_v2",
+        config=config,
+    )
+
+
+def dist_setup(rank, world_size):
+    # initialize the process group
+    dist.init_process_group("nccl", rank=rank, world_size=world_size)
+
+
+def dist_cleanup():
+    dist.destroy_process_group()
