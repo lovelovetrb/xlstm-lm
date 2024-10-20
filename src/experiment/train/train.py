8d38f1d400e37308773c08ae6ff24483c97a7fee
Mizuki <mbaba@kanolab.net>
2024-10-20 21:45:13 +0900

    refactor: ファイル名の変更と各モジュールを読み込む処理を別ファイルに切り出し

--- src/experiment/train/train.py
+++ src/experiment/train/train.py
@@ -1,87 +1,68 @@
+import argparse
+import logging
 import os
+from typing import Tuple
 
 import torch
+import torch.multiprocessing as mp
 import torch.optim as optim
-from dacite import Config as DaciteConfig
-from dacite import from_dict
-from omegaconf import OmegaConf
 from torch.utils.data import DataLoader
-
 from transformers import set_seed
-from xlstm import xLSTMLMModel, xLSTMLMModelConfig
 
 import wandb
+from src.cfg.config_type import ExperimentConfig
 from src.cfg.load_yaml_cfg import load_config
-from src.dataset.nlp_dataset import NlpDatasetGenerator
-from src.experiment.train.lr_scheduler import LinearWarmupCosineAnnealing
+from src.experiment.setup.criterion import setup_criterion
+from src.experiment.setup.dataset import setup_dataset
+from src.experiment.setup.lr_scheduler import setup_lr_scheduler
+from src.experiment.setup.model import setup_model
+from src.experiment.setup.optimizer import setup_optimizer
 from src.experiment.train.trainer import Trainer
-from src.utils import torch_dtype_map, wandb_init
+from src.utils import dist_cleanup, dist_setup, get_logger, wandb_init
 
 os.environ["TOKENIZERS_PARALLELISM"] = "false"
 
 
-def main(config):
+def setup_training(
+    rank: int, world_size: int, config: ExperimentConfig
+) -> Tuple[torch.nn.Module, DataLoader, optim.Optimizer, torch.nn.Module, torch.nn.Module]:
     wandb_init(config)
     set_seed(config.basic.seed)
+    dist_setup(rank, world_size)
 
-    pad_token_id = config.dataset.pad_token_id
+    train_loader = setup_dataset(config, rank, world_size)
+    model = setup_model(config, rank)
+    optimizer = setup_optimizer(model, config)
+    lr_scheduler = setup_lr_scheduler(optimizer, config)
+    criterion = setup_criterion(config)
 
-    dataset = NlpDatasetGenerator(config)
-    train_loader = DataLoader(
-        dataset.train,
-        batch_size=config.training.batch_size,
-        drop_last=True,
-        num_workers=1,
-        pin_memory=True,
-        shuffle=False,
-    )
-    print("Dataset loaded! ")
-    print(f"Train dataset size: {len(dataset.train)}")
+    return model, train_loader, lr_scheduler, optimizer, criterion
 
-    model_config = from_dict(
-        data_class=xLSTMLMModelConfig,
-        data=OmegaConf.to_container(config.model, resolve=True),
-        config=DaciteConfig(strict=True),
-    )
-    model = xLSTMLMModel(model_config)
-    model.reset_parameters()
-    model.train()
-    model = model.to(dtype=torch_dtype_map[config.training.weight_precision])
-    model = model.to(config.basic.device)
 
-    optim_groups = model._create_weight_decay_optim_groups()
-
-    optimizer = optim.AdamW(
-        (
-            {"weight_decay": config.training.weight_decay, "params": optim_groups[0]},
-            {"weight_decay": 0.0, "params": optim_groups[1]},
-        ),
-        lr=config.training.lr,
-        betas=(0.9, 0.95),
-        eps=1e-5,
-    )
-
-    lr_scheduler = LinearWarmupCosineAnnealing(
-        optimizer,
-        config.training.lr_warmup_steps,
-        config.training.lr_decay_until_steps,
-        config.training.lr,
-        config.training.lr_decay_factor * config.training.lr,
-    )
-
-    criterion = torch.nn.CrossEntropyLoss(ignore_index=pad_token_id)
-
-    trainer = Trainer(
-        model, train_loader, lr_scheduler, optimizer, criterion, config, rank=0
-    )
-    trainer.train()
-    print("Training finished!")
+def cleanup() -> None:
+    dist_cleanup()
     wandb.finish()
 
 
-if __name__ == "__main__":
-    config = load_config("src/cfg/yaml/v4/train_config.yaml")
+def main(rank: int, world_size: int, config: ExperimentConfig, logger: logging.Logger) -> None:
     try:
-        main(config)
+        model, train_loader, lr_scheduler, optimizer, criterion = setup_training(rank, world_size, config)
+        trainer = Trainer(model, train_loader, lr_scheduler, optimizer, criterion, config, rank, logger)
+        trainer.train()
     except Exception as e:
-        print(e)
+        logger.error(f"Error in main: {e}")
+    finally:
+        cleanup()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--config", type=str)
+    args = parser.parse_args()
+
+    logger = get_logger(__name__)
+    logger.info(f"Loading config from {args.config}")
+    config = load_config(args.config)
+    WORLD_SIZE = torch.cuda.device_count()
+
+    mp.spawn(main, args=(WORLD_SIZE, config, logger), nprocs=WORLD_SIZE, join=True)
