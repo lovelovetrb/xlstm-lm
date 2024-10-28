dde38d9ab66330b7c4eef8b8cc8123cff0e98a34
Mizuki <mbaba@kanolab.net>
2024-10-28 15:05:14 +0900

    chore: TODOコメントを追加

--- src/experiment/setup/criterion.py
+++ src/experiment/setup/criterion.py
@@ -4,4 +4,5 @@ from src.cfg.config_type import ExperimentConfig
 
 
 def setup_criterion(config: ExperimentConfig) -> torch.nn.Module:
+    # TODO: ignore_indexをtokenizerから取得するように変更
     return torch.nn.CrossEntropyLoss(ignore_index=config.dataset.pad_token_id)
--- src/experiment/setup/dataset.py
+++ src/experiment/setup/dataset.py
@@ -2,11 +2,17 @@ import torch
 from torch.utils.data import DataLoader
 
 from src.cfg.config_type import ExperimentConfig
-from src.dataset.nlp_dataset import DistributedIterableWrapper, NlpDatasetGenerator
+from src.dataset.nlp_dataset import (
+    DistributedIterableWrapper,
+    NlpDataset,
+    NlpDatasetGenerator,
+)
 
 
 def setup_dataset(config: ExperimentConfig, rank: int, world_size: int, subset: str) -> DataLoader:
-    dataset = NlpDatasetGenerator(config)[subset]
+    # TODO: DatasetGeneratorに対してtokenizerを渡すような実装に変更
+    dataset_generator = NlpDatasetGenerator(config)
+    dataset = get_dataset_subset(dataset_generator, subset)
     if config.training.use_fsdp:
         dataset = DistributedIterableWrapper(dataset, world_size, rank)
     return DataLoader(
@@ -17,3 +23,14 @@ def setup_dataset(config: ExperimentConfig, rank: int, world_size: int, subset: 
         pin_memory=True,
         shuffle=False,
     )
+
+
+def get_dataset_subset(data: NlpDatasetGenerator, subset: str) -> NlpDataset:
+    if subset == "train":
+        return data.train
+    elif subset == "valid":
+        return data.valid
+    elif subset == "test":
+        return data.test
+    else:
+        raise ValueError(subset)
