c1186393b0a04afc7a89f41c464c22194275a181
Mizuki <mbaba@kanolab.net>
2024-11-11 18:17:05 +0900

    chore: TODOに対するissueの追加

--- src/experiment/setup/model.py
+++ src/experiment/setup/model.py
@@ -5,6 +5,6 @@ from src.model.xlstm_model_wrapper import xLSTMModelWrapper
 
 
 # TODO: checkpointの読み込み処理
-# issue: https://github.com/lovelovetrb/xlstm-lm/issues/25
+# https://github.com/lovelovetrb/xlstm-lm/issues/25
 def setup_model(config: ExperimentConfig, rank: int, model_weight_path: str | None = None) -> torch.nn.Module:
     return xLSTMModelWrapper(config, rank, model_weight_path).get_model()
