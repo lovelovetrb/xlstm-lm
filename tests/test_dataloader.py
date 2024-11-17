6c4662c62ec20a1620dc49116e120aaa82f8aae2
Mizuki <mbaba@kanolab.net>
2024-11-17 23:07:44 +0900

    chore: データローダーのテストに対してマルチランクに対するテストを追加

--- tests/test_dataloader.py
+++ tests/test_dataloader.py
@@ -5,7 +5,7 @@ from src.experiment.setup.tokenizer import setup_tokenizer
 from tests.const import cfg_paths
 
 
-def test_dataloader(cfg_paths: dict) -> None:
+def test_dataloader_one_rank(cfg_paths: dict) -> None:
     # 1.3Bの設定を読み込む
     tokenizer = setup_tokenizer("gpt2")
 
@@ -46,5 +46,49 @@ def test_dataloader(cfg_paths: dict) -> None:
             print("\n\n")
         if not torch.equal(feature, first_iter[index]):
             raise ValueError("Dataloader is not reset.")
+        if index >= sample_num:
+            break
+
+
+def test_dataloader_two_rank(cfg_paths: dict) -> None:
+    # 1.3Bの設定を読み込む
+    tokenizer = setup_tokenizer("gpt2")
+
+    config = load_config(cfg_paths["1_3b"])
+    generator = get_dataset_generator(config, tokenizer)
+    dataloader_rank1 = setup_dataloader(generator, config, 0, 2, "train")
+    dataloader_rank2 = setup_dataloader(generator, config, 1, 2, "train")
+
+    sample_num = 50
+    first_iter_rank_1 = []
+    first_iter_rank_2 = []
+
+    for index, (batch1, batch2) in enumerate(zip(dataloader_rank1, dataloader_rank2)):
+        feature1, label1 = batch1["feature"][0], batch1["label"][0]
+        feature2, label2 = batch2["feature"][0], batch2["label"][0]
+        first_iter_rank_1.append(feature1)
+        first_iter_rank_2.append(feature2)
+        if torch.equal(feature1, feature2):
+            raise ValueError("Dataloader is not shuffled.")
+        if index >= sample_num:
+            break
+
+    # NOTE: データセットが続きから始まるかどうかを確認
+    print("Test1: Dataloader is reset.")
+    dataloader_rank1.dataset.set_start_index(sample_num)
+    dataloader_rank2.dataset.set_start_index(sample_num)
+    for index, (batch1, batch2) in enumerate(zip(dataloader_rank1, dataloader_rank2)):
+        feature1, label1 = batch1["feature"][0], batch1["label"][0]
+        feature2, label2 = batch2["feature"][0], batch2["label"][0]
+        if index == 10:
+            print(f"{index} : {tokenizer.decode(feature1, skip_special_tokens=True)}")
+            print(f"{index} : {tokenizer.decode(first_iter_rank_1[index], skip_special_tokens=True)}")
+            print("\n\n")
+        if torch.equal(feature1, first_iter_rank_1[index]):
+            raise ValueError("Dataloader is not shuffled.")
+        if torch.equal(feature2, first_iter_rank_2[index]):
+            raise ValueError("Dataloader is not shuffled.")
+        if torch.equal(feature1, feature2):
+            raise ValueError("Dataloader is not shuffled.")
         if index >= sample_num:
             break
