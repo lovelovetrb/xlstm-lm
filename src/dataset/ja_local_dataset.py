45a79a674b3b93a5b92cfbc22de72df6a2ddeace
Mizuki <info@lovelovetrb.com>
2025-01-08 18:20:41 +0900

    fix: 名称を変更・huggingfaceのdatasetと形式が同じになるように修正

---
+++ src/dataset/ja_local_dataset.py
@@ -0,0 +1,16 @@
+from datasets import load_dataset
+
+from src.cfg.config_type import ExperimentConfig
+
+
+class JaLocalDataset:
+    def __init__(self, cfg: ExperimentConfig, subset: str) -> None:
+        self.data = self._load_data(file_path=cfg.dataset.name, subset=subset)
+
+    def _load_data(self, file_path: str, subset: str) -> list[dict]:
+        dataset = load_dataset(
+            "text",
+            data_files={f"{subset}": [f"./src/dataset/data/{file_path}"]},
+            streaming=True,
+        )
+        return dataset[subset].map(lambda x: {"text": x["text"]})
