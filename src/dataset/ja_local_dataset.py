0bb0cd2cbe5fb904e480d27f2663694662e0f698
Mizuki <info@lovelovetrb.com>
2025-01-22 03:06:01 +0900

    feat: 複数GPUでの学習においてラムダ式が利用できない問題を解消

--- src/dataset/ja_local_dataset.py
+++ src/dataset/ja_local_dataset.py
@@ -7,10 +7,14 @@ class JaLocalDataset:
     def __init__(self, cfg: ExperimentConfig, subset: str) -> None:
         self.data = self._load_data(file_path=cfg.dataset.name, subset=subset)
 
+    @staticmethod
+    def process_data(x: dict) -> dict:
+        return {"text": x["text"]}
+
     def _load_data(self, file_path: str, subset: str) -> list[dict]:
         dataset = load_dataset(
             "text",
-            data_files={f"{subset}": [f"./src/dataset/data/{file_path}"]},
+            data_files={f"{subset}": [f"src/dataset/data/{file_path}"]},
             streaming=True,
         )
-        return dataset[subset].map(lambda x: {"text": x["text"]})
+        return dataset[subset].map(self.process_data)
